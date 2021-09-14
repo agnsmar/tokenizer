f79311297fa0d4667c9f3b0ae7bdf985d07612b6
Denni Wernersson <dw222fy@student.lnu.se>
2021-09-14 14:53:41 +0200

    Add automatic test case for example 2

--- test/Tokenizer.test.js
+++ test/Tokenizer.test.js
@@ -33,7 +33,24 @@ const ex1ResultString = ex1Result.map((token) => {
   return `\n\t${token.type}('${token.value}')`
 })
 
-describe('Example 1', () => {
+const ex2Result = [
+  {
+    type: 'NUMBER',
+    value: '3'
+  },
+  {
+    type: 'ADD',
+    value: '+'
+  },
+  {
+    type: 'NUMBER',
+    value: '2'
+  }]
+const ex2ResultString = ex2Result.map((token) => {
+  return `\n\t${token.type}('${token.value}')`
+})
+
+describe('Example 1 - wordAndDotGrammar', () => {
   describe('return value', () => {
     it(`'Meningen består av ord' should return ${ex1ResultString}`, () => {
       const word = new TokenType('Word', /^[\w|åäöÅÄÖ]+/)
@@ -45,6 +62,26 @@ describe('Example 1', () => {
       const textTokenizer = new Tokenizer(wordAndDotGrammar, 'Meningen består av ord.')
       textTokenizer.tokenize()
       expect(textTokenizer.getTokens()).to.eql(ex1Result)
+    })
+  })
+})
+
+describe('Example 2 - arithmeticGrammar', () => {
+  describe('return value', () => {
+    it(`'3 + 2' should return ${ex2ResultString}`, () => {
+      const number = new TokenType('NUMBER', /^[0-9]+(\.([0-9])+)?/)
+      const add = new TokenType('ADD', /^[+]/)
+      const mul = new TokenType('MUL', /^[*]/)
+      const arithmeticGrammar = new Grammar()
+      arithmeticGrammar.addTokenType(number)
+      arithmeticGrammar.addTokenType(add)
+      arithmeticGrammar.addTokenType(mul)
+
+      const arithmeticTokenizer = new Tokenizer(arithmeticGrammar, '3 + 2')
+
+      arithmeticTokenizer.tokenize()
+
+      expect(arithmeticTokenizer.getTokens()).to.eql(ex2Result)
     })
   })
 })
