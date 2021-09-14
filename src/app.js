1a7f306a5fcaf7bac2bea0e8c1589a4b179f6d2f
Denni Wernersson <dw222fy@student.lnu.se>
2021-09-14 14:31:55 +0200

    Add grammar to practice maximal munch

--- src/app.js
+++ src/app.js
@@ -2,12 +2,19 @@ import { Grammar } from './Grammar.js'
 import { Tokenizer } from './Tokenizer.js'
 import { TokenType } from './TokenType.js'
 
+// wordAndDotGrammar Example
 const word = new TokenType('WORD', /^[a-zA-Z|åäöÅÄÖ]+/)
 const dot = new TokenType('DOT', /^\./)
 const wordAndDotGrammar = new Grammar()
 wordAndDotGrammar.addTokenType(word)
 wordAndDotGrammar.addTokenType(dot)
 
+const textTokenizer = new Tokenizer(wordAndDotGrammar, 'Meningen består av ord.')
+
+textTokenizer.tokenize()
+console.table(textTokenizer.getTokens())
+
+// arithmeticGrammar Example
 const number = new TokenType('NUMBER', /^[0-9]+(\.([0-9])+)?/)
 const add = new TokenType('ADD', /^[+]/)
 const mul = new TokenType('MUL', /^[*]/)
@@ -16,12 +23,22 @@ arithmeticGrammar.addTokenType(number)
 arithmeticGrammar.addTokenType(add)
 arithmeticGrammar.addTokenType(mul)
 
-const textTokenizer = new Tokenizer(wordAndDotGrammar, 'Meningen består av ord.')
-
-textTokenizer.tokenize()
-console.table(textTokenizer.getTokens())
-
 const arithmeticTokenizer = new Tokenizer(arithmeticGrammar, '3.12 4 45 + 30 * 3')
 
 arithmeticTokenizer.tokenize()
 console.table(arithmeticTokenizer.getTokens())
+
+// MaximalMunchGrammar Example
+
+const float = new TokenType('FLOAT', /^[0-9]+\.[0-9]+/)
+const integer = new TokenType('INTEGER', /^[0-9]+/)
+
+const maximalMunchGrammar = new Grammar()
+
+maximalMunchGrammar.addTokenType(float)
+maximalMunchGrammar.addTokenType(integer)
+
+const maximalMunchTokenizer = new Tokenizer(maximalMunchGrammar, '3.14 5')
+
+maximalMunchTokenizer.tokenize()
+console.table(maximalMunchTokenizer.getTokens())
