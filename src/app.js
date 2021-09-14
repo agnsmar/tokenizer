dc540902b36886fb97e7590ee4711d306059a116
Denni Wernersson <dw222fy@student.lnu.se>
2021-09-14 12:46:06 +0200

    Add grammar for arithmetic with related tokentypes

--- src/app.js
+++ src/app.js
@@ -2,13 +2,26 @@ import { Grammar } from './Grammar.js'
 import { Tokenizer } from './Tokenizer.js'
 import { TokenType } from './TokenType.js'
 
-const word = new TokenType('Word', /^[\w|åäöÅÄÖ]+/)
-const dot = new TokenType('Dot', /^\./)
+const word = new TokenType('WORD', /^[\w|åäöÅÄÖ]+/)
+const dot = new TokenType('DOT', /^\./)
 const wordAndDotGrammar = new Grammar()
 wordAndDotGrammar.addTokenType(word)
 wordAndDotGrammar.addTokenType(dot)
 
-const testTokenizer = new Tokenizer(wordAndDotGrammar, 'Meningen består av ord.')
+const number = new TokenType('NUMBER', /^[0-9]+(\.([0-9])+)?/)
+const add = new TokenType('ADD', /^[+]/)
+const mul = new TokenType('MUL', /^[*]/)
+const arithmeticGrammar = new Grammar()
+arithmeticGrammar.addTokenType(number)
+arithmeticGrammar.addTokenType(add)
+arithmeticGrammar.addTokenType(mul)
 
-testTokenizer.tokenize()
-console.table(testTokenizer.getTokens())
+const textTokenizer = new Tokenizer(wordAndDotGrammar, 'Meningen består av ord.')
+
+textTokenizer.tokenize()
+console.table(textTokenizer.getTokens())
+
+const arithmeticTokenizer = new Tokenizer(arithmeticGrammar, '3.12 4 45 + 30 * 3')
+
+arithmeticTokenizer.tokenize()
+console.table(arithmeticTokenizer.getTokens())
