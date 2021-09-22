55ed3c9acf3995af385c261fff07f2aa9823fbc6
Denni Wernersson <dw222fy@student.lnu.se>
2021-09-22 12:52:51 +0200

    Rewrite example grammars without helper classes

--- test/exampleGrammars.js
+++ test/exampleGrammars.js
@@ -1,35 +1,22 @@
-import { Grammar } from '../src/Grammar.js'
-import { TokenType } from '../src/TokenType.js'
-
 // Word and Dot grammar.
-const word = new TokenType('WORD', /^[a-zA-Z|åäöÅÄÖ]+/)
-const dot = new TokenType('DOT', /^\./)
-export const wordAndDot = new Grammar()
-wordAndDot.addTokenType(word)
-wordAndDot.addTokenType(dot)
+export const wordAndDot = [
+  { name: 'WORD', regex: /^[a-zA-Z|åäöÅÄÖ]+/ },
+  { name: 'DOT', regex: /^\./ }
+]
 
 // Arithmetic Grammar.
-const number = new TokenType('NUMBER', /^[0-9]+(\.([0-9])+)?/)
-const add = new TokenType('ADD', /^[+]/)
-const mul = new TokenType('MUL', /^[*]/)
-const sub = new TokenType('SUB', /^[-]/)
-const div = new TokenType('DIV', /^[/]/)
-const openPar = new TokenType('OPENPAR', /^[(]/)
-const closePar = new TokenType('CLOSEPAR', /^[)]/)
-export const arithmetic = new Grammar()
-arithmetic.addTokenType(number)
-arithmetic.addTokenType(add)
-arithmetic.addTokenType(mul)
-arithmetic.addTokenType(sub)
-arithmetic.addTokenType(div)
-arithmetic.addTokenType(openPar)
-arithmetic.addTokenType(closePar)
+export const arithmetic = [
+  { name: 'NUMBER', regex: /^[0-9]+(\.([0-9])+)?/ },
+  { name: 'ADD', regex: /^[+]/ },
+  { name: 'MUL', regex: /^[*]/ },
+  { name: 'SUB', regex: /^[-]/ },
+  { name: 'DIV', regex: /^[/]/ },
+  { name: 'OPENPAR', regex: /^[(]/ },
+  { name: 'CLOSEPAR', regex: /^[)]/ }
+]
 
 // Maximal Munch Grammar.
-const float = new TokenType('FLOAT', /^[0-9]+\.[0-9]+/)
-const integer = new TokenType('INTEGER', /^[0-9]+/)
-
-export const maximalMunch = new Grammar()
-
-maximalMunch.addTokenType(float)
-maximalMunch.addTokenType(integer)
+export const maximalMunch = [
+  { name: 'FLOAT', regex: /^[0-9]+\.[0-9]+/ },
+  { name: 'INTEGER', regex: /^[0-9]+/ }
+]
