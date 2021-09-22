a63c02fa29d4bddcfd72a8366955e4fd5e12c28c
Denni Wernersson <dw222fy@student.lnu.se>
2021-09-22 14:52:34 +0200

    Add input getter to consoleUI

--- test/view/ConsoleUI.js
+++ test/view/ConsoleUI.js
@@ -1,21 +1,43 @@
-import readline from 'readline-sync'
+import readlineSync from 'readline-sync'
+import readline from 'readline'
 
 export class ConsoleUI {
+  constructor () {
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    })
+  }
+
   printWelcome () {
     console.log('Welcome to the Tokenizer lmao')
   }
 
   printActiveToken (token) {
-    console.clear()
     console.log(`--Active Token--\nType:\t${token.type}\nValue:\t${token.value}`)
+  }
+
+  clearConsole () {
+    console.clear()
+  }
+
+  printString (string) {
+    console.log(string)
   }
 
   printIndex (index) {
     console.log(`Currently observing token[${index + 1}]`)
   }
 
+  async getInputStringToTokenize () {
+    return new Promise(resolve => this.rl.question('input: ', ans => {
+      this.rl.close()
+      resolve(ans)
+    }))
+  }
+
   getInputChar (MOVER_CHARS) {
-    const key = readline.keyIn(`\nPress [${MOVER_CHARS.back}] to go back, [${MOVER_CHARS.next}] to go forward. Any other key to quit...\n`)
+    const key = readlineSync.keyIn(`\nPress [${MOVER_CHARS.back}] to go back, [${MOVER_CHARS.next}] to go forward. Any other key to quit...\n`)
 
     if (key !== MOVER_CHARS.back && key !== MOVER_CHARS.next) {
       return false
